956d6a90c33f5575442ae2c30de5d33b88a8bd63
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-25 02:35:42 +0300

    add count vowels problem solution

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,14 @@
-function countVowels() {}
+function countVowels(str) {
+  cnt = 0;
+  vowels = ['a', 'i', 'o', 'e', 'u'];
+
+  for (const char of str.toLowerCase()) {
+    if (vowels.includes(char)) {
+      cnt += 1;
+    }
+  }
+
+  return cnt;
+}
 
 module.exports = countVowels;
