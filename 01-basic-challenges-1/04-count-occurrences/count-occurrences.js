b70082b72d47d13798d748a67e8d885dd63d50a2
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-24 16:32:14 +0300

    solve count occurrences problem

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,13 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  let cnt = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (char == str[i]) {
+      cnt += 1;
+    }
+  }
+
+  return cnt;
+}
 
 module.exports = countOccurrences;
