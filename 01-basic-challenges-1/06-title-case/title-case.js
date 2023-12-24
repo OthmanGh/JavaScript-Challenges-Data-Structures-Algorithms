128e673ae7cc4d96393895c47eef3ce57a16c26e
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-24 16:47:38 +0300

    add title case solution

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,15 @@
-function titleCase() {}
+function titleCase(str) {
+  formattedStr = [];
+
+  words = str.split(' ');
+
+  for (const word of words) {
+    formattedWord = word[0].toUpperCase() + word.slice(1).toLowerCase();
+
+    formattedStr.push(formattedWord);
+  }
+
+  return formattedStr.join(' ');
+}
 
 module.exports = titleCase;
