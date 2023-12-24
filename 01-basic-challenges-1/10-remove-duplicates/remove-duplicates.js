75f359f5c2787138e82d800834c9ddb66e0ea527
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-25 02:46:36 +0300

    add remove duplicates problem solution

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,19 @@
-function removeDuplicates() {}
+// ! Solution 1 :
+// function removeDuplicates(arr) {
+//   set = new Set(arr);
+//   return Array.from(set);
+// }
+
+// ! Solution 2 :
+function removeDuplicates(arr) {
+  uniqueArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!uniqueArr.includes(arr[i])) {
+      uniqueArr.push(arr[i]);
+    }
+  }
+
+  return uniqueArr;
+}
 
 module.exports = removeDuplicates;
