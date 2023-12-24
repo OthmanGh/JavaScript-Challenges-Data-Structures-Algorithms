ab46c05ef9abc3b8e06f9c3989160e67774a8415
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-24 16:25:03 +0300

    solve calculator problem

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,28 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  let result = 0;
+  switch (operator) {
+    case '+':
+      result = num1 + num2;
+      break;
+
+    case '-':
+      result = num1 - num2;
+      break;
+
+    case '*':
+      result = num1 * num2;
+      break;
+
+    case '/':
+      result = num1 / num2;
+      break;
+
+    default:
+      result = 'Invalid operator';
+      break;
+  }
+
+  return result;
+}
 
 module.exports = calculator;
