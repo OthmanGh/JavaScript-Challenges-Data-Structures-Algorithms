3b274b5448544f3382b06aedccf19af3b9071d59
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-24 20:41:44 +0300

    add reverse string problem solution

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,32 @@
-function reverseString() {}
+function reverseString(str) {
+  // s1 :
+  //   let reversedStr = '';
+  //   for (let i = str.length - 1; i >= 0; i--) {
+  //     reversedStr += str[i];
+  //   }
+
+  //   return reversedStr;
+
+  // s2 :
+  //string are immutable in js
+  strArr = str.split('');
+
+  let s = 0;
+  let e = strArr.length - 1;
+
+  while (s < e) {
+    temp = strArr[s];
+    strArr[s] = strArr[e];
+    strArr[e] = temp;
+    s++;
+    e--;
+  }
+  return strArr.join('');
+
+  // s3 :
+  // return str.split('').reverse().join('');
+}
+
+console.log(reverseString('hello'));
 
 module.exports = reverseString;
