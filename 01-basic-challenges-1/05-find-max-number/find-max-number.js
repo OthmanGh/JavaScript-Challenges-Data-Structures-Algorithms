29ed223da312f2202dd8f7bbd7b6b2111376d0c6
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-24 16:37:08 +0300

    add find max number solution

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,12 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  // return Math.max(...arr);
+
+  max = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    max < arr[i] ? (max = arr[i]) : max;
+  }
+  return max;
+}
 
 module.exports = findMaxNumber;
