1a9d4414166ab02ad90a44299bf0f59b52e46fcb
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-26 01:32:32 +0300

    add find missing letter problem solution

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,18 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  let prevCode = arr[0].charCodeAt(0);
+
+  for (let i = 1; i < arr.length; i++) {
+    const currentCode = arr[i].charCodeAt(0);
+    const diff = currentCode - prevCode;
+
+    if (diff !== 1) {
+      return String.fromCharCode(prevCode + 1);
+    }
+
+    prevCode = currentCode;
+  }
+
+  return '';
+}
 
 module.exports = findMissingLetter;
