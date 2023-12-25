b9e392020a4a1fac4091b8f5b4af51d09c00d256
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-26 01:59:35 +0300

    add first non-repeating problem solution

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,16 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+  const charCount = {};
+
+  for (const char of str) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  for (const [key, value] of Object.entries(charCount)) {
+    if (value == 1) {
+      return key;
+    }
+  }
+  return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
