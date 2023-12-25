1a5b4ae93fbc91ae1b4097c2dabf289007dd9e09
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-26 01:26:29 +0300

    add find missing number problem solution

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,13 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  const n = arr.length + 1;
+  const totalSum = (n * (n + 1)) / 2;
+
+  let currentSum = 0;
+  for (const num of arr) {
+    currentSum += num;
+  }
+
+  return totalSum - currentSum;
+}
 
 module.exports = findMissingNumber;
