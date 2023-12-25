c726986002e171444b3de5f87be05bb6b74a67e4
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-26 01:37:58 +0300

    add are all characters unique problem solution

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,15 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  const uniqueChars = [];
+
+  for (const char of str) {
+    if (uniqueChars.includes(char)) {
+      return false;
+    }
+
+    uniqueChars.push(char);
+  }
+
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
