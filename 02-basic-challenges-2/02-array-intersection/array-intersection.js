ce6d86c5a55e3f27b12c10c03a43b51a6ef64354
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-26 01:12:09 +0300

    add array intersection problem solution

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,13 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  const intersectionArr = [];
+
+  for (const el of arr1) {
+    if (arr2.includes(el)) {
+      intersectionArr.push(el);
+    }
+  }
+
+  return intersectionArr;
+}
 
 module.exports = arrayIntersection;
