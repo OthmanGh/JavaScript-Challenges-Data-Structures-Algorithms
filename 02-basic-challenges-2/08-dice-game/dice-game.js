a591f0f72fe3f81e677f69b95c664cc8526a4a8b
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-26 08:21:21 +0300

    add dice game challenge solution

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,27 @@
-function diceGameSimulation() {}
+const getRandom = () => Math.floor(Math.random() * 6) + 1;
+
+function diceGameSimulation(simulation) {
+  const output = [];
+  for (let i = 0; i < simulation; i++) {
+    const dice1 = getRandom();
+    const dice2 = getRandom();
+
+    sum = dice1 + dice2;
+
+    let result = '';
+
+    if (sum === 7 || sum === 11) {
+      result = 'win';
+    } else if (sum === 2 || sum === 3 || sum === 12) {
+      result = 'lose';
+    } else {
+      result = 'roll again';
+    }
+
+    output.push({ dice1: dice1, dice2: dice2, sum: sum, result: result });
+  }
+
+  return output;
+}
 
 module.exports = diceGameSimulation;
