03f067a71742e04cb40c42009a31e4217bbaa9b7
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-27 17:10:50 +0300

    add valid ipv4 challenge solution

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,11 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (str) => {
+  const octets = str.split('.');
+
+  if (octets.length !== 4) return false;
+
+  return octets.every(
+    (octet) => octet >= 0 && octet <= 255 && octet.charAt(0) != 0
+  );
+};
 
 module.exports = isValidIPv4;
