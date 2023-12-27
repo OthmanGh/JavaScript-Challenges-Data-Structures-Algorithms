489c4092a741179db179d8f052587154c9db9373
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-27 19:46:13 +0300

    add password validator challenge solution

--- 03-high-order-array-methods/09-password-validator/password-validator-run.js
+++ 03-high-order-array-methods/09-password-validator/password-validator-run.js
@@ -1,6 +1,7 @@
 const validatePassword = require('./password-validator');
 
-const result1 = validatePassword('Abc12345');
-const result2 = validatePassword('password');
+const result1 = validatePassword('abc12345');
+const result2 = validatePassword('passwordAs5');
 
-console.log(result1, result2);
+console.log(result1);
+console.log(result2);
--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,18 @@
-function validatePassword() {}
+function validatePassword(password) {
+  if (password.length < 8) return false;
+
+  const hasCapitalChar = password
+    .split('')
+    .some((char) => char === char.toLowerCase() && char !== char.toUpperCase());
+
+  const hasSmallChar = password
+    .split('')
+    .some((char) => char === char.toUpperCase() && char !== char.toLowerCase());
+
+  const hasDigit = password
+    .split('')
+    .some((char) => !isNaN(parseInt(char, 10)));
+  return hasCapitalChar && hasSmallChar && hasDigit;
+}
 
 module.exports = validatePassword;
