44bed8d176ab6b443853c8f5213c460ce5a5900a
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-27 15:25:51 +0300

    add hashtag generator challenge solution

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,28 @@
-function generateHashtag() {}
+// Solution : 1
+// function generateHashtag(str) {
+//   if (str.length > 140 || str.length == 0 || str.trim() === '') {
+//     return false;
+//   }
 
+//   const formattedStr = str
+//     .trim()
+//     .split(/\s+/)
+//     .reduce((s, word) => {
+//       word = word[0].toUpperCase() + word.substring(1);
+//       s += word;
+//       return s;
+//     }, '');
+
+//   return '#' + formattedStr;
+// }
+
+function generateHashtag(str) {
+  const hashtag = str
+    .trim()
+    .split(/\s+/)
+    .reduce((tag, word) => {
+      return tag + word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    }, '#');
+  return hashtag.length == 1 || hashtag.length > 140 ? false : hashtag;
+}
 module.exports = generateHashtag;
