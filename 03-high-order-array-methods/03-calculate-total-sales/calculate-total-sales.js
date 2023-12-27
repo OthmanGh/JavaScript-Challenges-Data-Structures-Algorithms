523c6d77adffc66b697a35687b2091fbfc0cc599
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-27 11:54:43 +0300

    add calculate total sales challenge solution

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,10 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+  const totalSales = products
+    .map((obj) => obj['price'] * obj['quantity'])
+    .reduce((total, price) => total + price, 0);
+
+  const totalSalesWithTax = (totalSales * taxRate) / 100;
+  return Number(totalSalesWithTax.toFixed(2));
+}
 
 module.exports = calculateTotalSalesWithTax;
