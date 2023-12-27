dbd7e09cb643ac8de13dad0beb4015e972c58ee8
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-27 19:18:05 +0300

    add analyze car milage challenge solution

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,28 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+  const totalMileage = cars.reduce((total, obj) => total + obj['mileage'], 0);
+
+  const averageMileage = parseFloat((totalMileage / cars.length).toFixed(2));
+
+  const lowestMileageCar = cars.reduce(
+    (lowest, car) =>
+      lowest['mileage'] > car['mileage'] ? (lowest = car) : lowest,
+    cars[0]
+  );
+
+  // different way
+  let highestMileageCar = cars[0];
+  cars.forEach((car) => {
+    if (car['mileage'] > highestMileageCar['mileage']) {
+      highestMileageCar = car;
+    }
+  });
+
+  return {
+    averageMileage: averageMileage,
+    highestMileageCar: highestMileageCar,
+    lowestMileageCar: lowestMileageCar,
+    totalMileage: totalMileage,
+  };
+}
 
 module.exports = analyzeCarMileage;
