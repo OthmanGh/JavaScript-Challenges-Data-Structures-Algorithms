4ffa4d51f893a8f76a725c108bcc8950a9cf0f85
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-28 20:56:41 +0300

    add count down problem solution

--- 04-recursion/01-recursion-intro/count-down.js
+++ 04-recursion/01-recursion-intro/count-down.js
@@ -1,3 +1,11 @@
-function countDown() {}
+function countDown(number) {
+  if (number <= 0) {
+    console.log('All done!');
+    return;
+  }
+
+  console.log(number);
+  countDown(number - 1);
+}
 
 module.exports = countDown;
